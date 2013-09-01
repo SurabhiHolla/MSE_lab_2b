c4922e087ac0efce05ca833d6107d115f58fff01
SurabhiHolla <hollasurabhi.93@gmail.com>
2013-09-01 11:26:22 +0530

    Create Arithmetic.h

---
+++ Arithmetic.h
@@ -0,0 +1,16 @@
+#import<Foundation/Foundation.h>
+
+@interface Add:NSObject
+{
+float a;
+float b;
+
+}
+-(void)show;
+-(void)setA:(float)s;
+-(void)setB:(float)v;
+-(void)add;
+-(void)sub;
+-(void)mul;
+-(void)div;
+@end
